32c756e1ca1212bec632bfe720af801228e619ec
Thien <thienhn4@uci.edu>
2019-08-01 10:03:31 -0700

    More printing fix for easier debugging
    Need to IMPLEMENT FILTERS and RECOVERING METHODS
    Need to OBTAIN BETTER NODE ORDERING

--- uclasmcode/candidate_structure/partial_match.py
+++ uclasmcode/candidate_structure/partial_match.py
@@ -6,70 +6,69 @@ from .supernodes import Supernode, SuperTemplateNode
 
 
 class PartialMatch(object):
-    """ This data structure stores the matching between template nodes and world nodes
-     It provides utilities like:
-        - rm_last_match (for the DFS tree to minimize space)
-        - rm_match      (for removing a match)
-        - get_matches   (returns a dictionary of Supernode and matched world node)
-        - add_match     (add a new match and keeps track of the order of adding) """
+	""" This data structure stores the matching between template nodes and world nodes
+	 It provides utilities like:
+		- rm_last_match (for the DFS tree to minimize space)
+		- rm_match      (for removing a match)
+		- get_matches   (returns a dictionary of Supernode and matched world node)
+		- add_match     (add a new match and keeps track of the order of adding) """
 
-    def __init__(self):
-        """ """
-        # dictionary of Supernode to a set of matched nodes of same size as supernode
-        # #Note that __hash__ is defined in Supernode
-        self.matches: {SuperTemplateNode: Supernode} = {}
-        self.node_stack = []  # a stack of last added SuperTemplateNodes
-        self.already_matched_world_nodes = set()  # for checking alldiff
+	def __init__(self):
+		""" """
+		# dictionary of Supernode to a set of matched nodes of same size as supernode
+		# #Note that __hash__ is defined in Supernode
+		self.matches: {SuperTemplateNode: Supernode} = {}
+		self.node_stack = []  # a stack of last added SuperTemplateNodes
+		self.already_matched_world_nodes = set()  # for checking alldiff
 
-    # ========== METHODS ===========
-    def rm_last_match(self) -> None:
-        """ pop the last match from the stack and remove the other stuff"""
-        last_super_node = self.node_stack.pop()
-        self.already_matched_world_nodes -= set(self.matches.pop(last_super_node).get_vertices())
+	# ========== METHODS ===========
+	def rm_last_match(self) -> None:
+		""" pop the last match from the stack and remove the other stuff"""
+		last_super_node = self.node_stack.pop()
+		self.already_matched_world_nodes -= set(self.matches.pop(last_super_node).get_vertices())
 
-    def rm_match(self, name_of_node: SuperTemplateNode) -> None:
-        """ Given a supernode remove it from the matches
-            can be a risky method """
-        assert name_of_node in self.matches, "PartialMatch.rm_match: Trying to remove a node (%s) that has not \
-                                            been matched. "
-        pass
+	def rm_match(self, name_of_node: SuperTemplateNode) -> None:
+		""" Given a supernode remove it from the matches
+			can be a risky method """
+		assert name_of_node in self.matches, "PartialMatch.rm_match: Trying to remove a node (%s) that has not been matched."
+		pass
 
-    def add_match(self, supernode: SuperTemplateNode, candidate_node: Supernode):
-        """ add the new_match to the matches. """
-        assert supernode not in self.matches, "PartialMatch.add_match: Trying to add an already added node"
-        print_info(f"Adding ({str(supernode.name)},{str(candidate_node.name)}) to {str(self)}\n")
-        assert len(supernode) == len(candidate_node), "Invalid matching: matching must be a set of equal len"
-        # push the new matches onto the stack
-        self.node_stack.append(supernode)
-        self.matches[supernode] = candidate_node
-        self.already_matched_world_nodes.update(candidate_node.get_vertices())
+	def add_match(self, supernode: SuperTemplateNode, candidate_node: Supernode):
+		""" add the new_match to the matches. """
+		assert supernode not in self.matches, "PartialMatch.add_match: Trying to add an already added node"
+		print_info(f"Adding ({str(supernode.name)},{str(candidate_node.name)}) to {str(self)}\n")
+		assert len(supernode) == len(candidate_node), "Invalid matching: matching must be a set of equal len"
+		# push the new matches onto the stack
+		self.node_stack.append(supernode)
+		self.matches[supernode] = candidate_node
+		self.already_matched_world_nodes.update(candidate_node.get_vertices())
 
-    # =========== QUERIES ==========
-    def get_matches(self) -> {SuperTemplateNode: {Supernode}}:
-        """ Returns a dictionary of matches of this partial match"""
-        return self.matches
+	# =========== QUERIES ==========
+	def get_matches(self) -> {SuperTemplateNode: {Supernode}}:
+		""" Returns a dictionary of matches of this partial match"""
+		return self.matches
 
-    def print_match_stack(self) -> str:
-        """ Return a nicely formatted match stack for debugging mainly """
-        return str([str(i) for i in self.node_stack])
+	def print_match_stack(self) -> str:
+		""" Return a nicely formatted match stack for debugging mainly """
+		return str([str(i) for i in self.node_stack])
 
-    # === utils ====
-    def __str__(self):
-        """ Gives the string of the matched dictionary """
-        return str([(str(u.name), str(v.name)) for u, v in self.matches.items()])
+	# === utils ====
+	def __str__(self):
+		""" Gives the string of the matched dictionary """
+		return str([(str(u.name), str(v.name)) for u, v in self.matches.items()])
 
-    def __repr__(self):
-        """ Gives some useful info for debugging """
-        result = self.print_match_stack()
-        result += "\nMatches dictionary: {}".format(
-            str({str(u): str(v) for u, v in self.matches.items()}))
-        return result
+	def __repr__(self):
+		""" Gives some useful info for debugging """
+		result = self.print_match_stack()
+		result += "\nMatches dictionary: {}".format(
+			str({str(u): str(v) for u, v in self.matches.items()}))
+		return result
 
-    def __eq__(self, other):    # mainly for debugging
-        return self.matches == other.matches and self.node_stack == other.node_stack
+	def __eq__(self, other):  # mainly for debugging
+		return self.matches == other.matches and self.node_stack == other.node_stack
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+	def __ne__(self, other):
+		return not self.__eq__(other)
 
-    def __len__(self):
-        return len(self.matches)
+	def __len__(self):
+		return len(self.matches)
--- uclasmcode/candidate_structure/simple_utils.py
+++ uclasmcode/candidate_structure/simple_utils.py
@@ -25,88 +25,88 @@ VERBOSE = True  # set the flag to True for verbose
 
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 
 def get_now():
-    """ Get a str of current time"""
-    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	""" Get a str of current time"""
+	return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 # ## Trivial utils
 def print_debug(msg):
-    """ Logging and stuff """
-    # logger.debug(msg)
-    if DEBUG:
-        print(f"{bcolors.WARNING}DEBUG:{bcolors.ENDC}", msg)
+	""" Logging and stuff """
+	# logger.debug(msg)
+	if DEBUG:
+		print(f"{bcolors.WARNING}DEBUG:{bcolors.ENDC}", msg)
 
 
 def print_info(msg: str):
-    logger.info(msg)
-    if VERBOSE:
-        print(f"{bcolors.BOLD}UPDATE:{bcolors.ENDC}", msg)
+	logger.info(msg)
+	if VERBOSE:
+		print(f"{bcolors.BOLD}UPDATE:{bcolors.ENDC}", msg)
 
 
 def get_dict_str(d: dict) -> str:
-    """ Given a dictionary, give a str output of key and value """
-    return str({str(u): str(v) for u, v in d.items()})
+	""" Given a dictionary, give a str output of key and value """
+	return str({str(u): str(v) for u, v in d.items()})
 
 
 def get_itr_str(iterable) -> str:
-    """ Given a list, set or tuple returns str of values """
-    return str([str(i) for i in iterable])
+	""" Given a list, set or tuple returns str of values """
+	return str([str(i) for i in iterable])
 
 
 def print_stats(partition, graph, detailed=True, name=False):
-    equiv_classes = partition.classes()
-    # compute some stats
-    print("==== GENERAL-STATS =====")
-    # the number of nodes
-    print("Total number of original nodes: " + str(len(partition)))
-    # the number of equiv classes
-    print("Total number of equiv classes: " + str(len(equiv_classes)))
-    # compression percentage
-    compression_percentage = 1 - len(equiv_classes) / len(partition)
-    print("Compression percentage (1 - equiv_classes/total_number_of_nodes): %4.2f" % (compression_percentage))
-    equiv_classes = partition.classes()
-    equiv_classes_name = []
-    for s in equiv_classes:
-        equiv_classes_name.append({graph.nodes[i] for i in s})
-    if detailed:
-        print("===== EQUIVALENCE CLASSES ===== ")
-        if name:
-            print(equiv_classes_name)
-        else:
-            print(equiv_classes)
+	equiv_classes = partition.classes()
+	# compute some stats
+	print("==== GENERAL-STATS =====")
+	# the number of nodes
+	print("Total number of original nodes: " + str(len(partition)))
+	# the number of equiv classes
+	print("Total number of equiv classes: " + str(len(equiv_classes)))
+	# compression percentage
+	compression_percentage = 1 - len(equiv_classes) / len(partition)
+	print("Compression percentage (1 - equiv_classes/total_number_of_nodes): %4.2f" % (compression_percentage))
+	equiv_classes = partition.classes()
+	equiv_classes_name = []
+	for s in equiv_classes:
+		equiv_classes_name.append({graph.nodes[i] for i in s})
+	if detailed:
+		print("===== EQUIVALENCE CLASSES ===== ")
+		if name:
+			print(equiv_classes_name)
+		else:
+			print(equiv_classes)
 
-    print("===== NON-TRIVIAL EQUIV CLASSES ===== ")
-    non_triv = [i for i in equiv_classes if len(i) > 1]
-    non_triv_name = []
-    for s in non_triv:
-        non_triv_name.append({graph.nodes[i] for i in s})
-    if detailed:
-        if name:
-            print(non_triv_name)
-        else:
-            print(non_triv)
-    print("Number of non-trivial equiv classes: %d" % len(non_triv))
+	print("===== NON-TRIVIAL EQUIV CLASSES ===== ")
+	non_triv = [i for i in equiv_classes if len(i) > 1]
+	non_triv_name = []
+	for s in non_triv:
+		non_triv_name.append({graph.nodes[i] for i in s})
+	if detailed:
+		if name:
+			print(non_triv_name)
+		else:
+			print(non_triv)
+	print("Number of non-trivial equiv classes: %d" % len(non_triv))
 
-    sizes = [len(l) for l in non_triv]
-    if detailed:
-        print("Sizes of equiv classes: " + str(sizes))
-        print("Largest equiv size: %d" % (max(sizes)))
+	sizes = [len(l) for l in non_triv]
+	if detailed:
+		print("Sizes of equiv classes: " + str(sizes))
+		print("Largest equiv size: %d" % (max(sizes)))
 
 
 def compute_non_trivial(adj_matrix):
-    result = []
-    for i in range(adj_matrix.shape[0]):
-        if adj_matrix[i].any() or adj_matrix[:, i].any():
-            result.append(i)
-    return result
+	result = []
+	for i in range(adj_matrix.shape[0]):
+		if adj_matrix[i].any() or adj_matrix[:, i].any():
+			result.append(i)
+	return result
