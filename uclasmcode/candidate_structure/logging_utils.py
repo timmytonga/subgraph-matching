8d23bfc3599a37fb76eeb03c7112ebdc8e257c0e
Thien <thienhn4@uci.edu>
2019-08-07 14:00:50 -0700

    - Removed newline in logger.

--- uclasmcode/candidate_structure/logging_utils.py
+++ uclasmcode/candidate_structure/logging_utils.py
@@ -16,7 +16,7 @@ def init_logger(log_level=logging.INFO):
 	LOGFILE_NAME = '{}.log'.format(NAME)  # TODO: Set this to correct format when logging
 	# prints log to stdout and also saves to specified log file
 	fh = RotatingFileHandler(LOGFILE_NAME, mode='w', maxBytes=500 * 1024 * 1024, backupCount=10, encoding=None, delay=0)
-	formatter = logging.Formatter('%(asctime)s - %(levelname)s:\n\t%(message)s')
+	formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
 	fh.setFormatter(formatter)
 	logger.addHandler(fh)
 	# ## UNCOMMENT BELOW FOR STDOUT
